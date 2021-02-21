91d13cabc1c5eef3df00a7a1106cc945e348ad64
xutai <514292663@qq.com>
2021-02-21 17:59:18 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/clion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clion.dir/main.cpp.obj"
+  "clion.exe"
+  "clion.exe.manifest"
+  "clion.lib"
+  "clion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/clion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+//    std::cout << "Hello, World!" << std::endl;
+//    return 0;
+
+//    std::cout << "Enter two numbers:" << std::endl;
+//    int v1 = 0, v2 = 0;
+//    std::cin >> v1 >> v2;
+//    std::cout << "The sum of " << v1 << " and " << v2
+//              << " is " << v1 + v2 << std::endl;
+
+//std::cout << "you";
+//std::cout << "are";
+//std::cout << std::endl;
+//std::cout << "who";
+
+//int v1 = 0, v2 = 0;
+//std::cin >> v1;
+//std::cin >> v2;
+//std::cout << v1;
+//std::cout << std::endl;
+//std::cout << v2;
+}
